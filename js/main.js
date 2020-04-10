b3cc7fe772a99939c6e784f0e66a8d35f9fc214c
pavelavramenko <olimpiyskiy1923>
2020-04-10 17:23:34 +0300

    +added menu

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(function(){
         autoplay: 3000,
         dots: true,
     });
+
+    $('.header-btn').on('click', function() {
+        $('.nav').addClass('active');
+    })
+
+    $('.nav-btn').on('click', function() {
+        $('.nav').removeClass('active');
+    })
+
 });
